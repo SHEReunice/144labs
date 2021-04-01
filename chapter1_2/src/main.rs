ac8cefd0049f788da334cdafb19847bd483826c3
SHEReunice <3061878210@qq.com>
2021-04-01 11:10:29 +0800

    chapter1&2

--- chapter1_2/src/main.rs
+++ chapter1_2/src/main.rs
@@ -100,6 +100,78 @@ fn main() {
     println!("{}", x);
    }
    */
-  
+
+   /*
+   //match关键字，用于匹配值的所有可能条件，并在匹配为真时执行相应代码，类似C里面的switch
+   let x = 42;
+   match x {
+       0 => {
+           println!("found zero");
+       }
+       //我们可以匹配多个值
+       1 | 2 => {
+           println!("found 1 or 2");
+       }
+       //可以匹配迭代器
+       3..=9 => {
+           println!("found a number 3 to 9 inclusive");
+       }
+       //可以匹配数值绑定到变量
+       matched_num @ 10..=100 => {
+           println!("found {} number between 10 to 100!", matched_num);
+       }
+       //这是默认匹配，如果没有处理所有情况，则必须存在该匹配
+       _ => {
+           println!("found something else!");
+       }
+   }
+   */
+
+   /*
+   //loop可以被中断返回一个值
+   let mut x = 0;
+   let v = loop {
+       x += 1;
+       if x == 13 {
+           break "found the 13";
+       }
+   };
+  println!("from loop: {}", v);
+  */
+
+  /*
+  //从块表达式返回值,如果if、match、函数或作用域块中的最后一条语句是不带;的表达式，Rust将把它作为一个值从块中返回。
+   println!("from function: {}", example());
+   */
+
+   
     
 }
+
+/*
+fn example() -> i32 {
+    let x = 42;
+    //Rust的三元表达式
+    let v = if x < 42 { -1 } else { 1 };
+    println!("from if: {}", v);
+
+    let food = "hamburger";
+    let result = match food {
+        "hotdog" => "is hotdog",
+        //注意，当它只是一个返回表达式时，大括号是可选的
+        _ => "is not hotdog",
+    };
+    println!("identifying food: {}", result);
+
+    let v = {
+        //这个作用域块让我们得到一个不影响函数作用域的结果
+        let a = 1;
+        let b = 2;
+        a+b
+    };
+    println!("from block: {}", v);
+
+    //在最后从函数返回值的惯用方法
+    v + 4
+}
+*/
